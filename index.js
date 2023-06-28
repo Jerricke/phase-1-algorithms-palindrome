0fb2028b4f723fbab1e95eac11d048740aad0db4
Jerricke <eejerrickw@gmail.com>
2023-06-28 16:35:53 -0600

    complete

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  const reverse = [];
+  for (let i = 0;  i < word.length; i++) {
+    reverse.unshift(word[i])
+  } 
+  const palindrome = reverse.join('');
+  if (palindrome === word) {
+    return true
+  } else return false
 }
 
+console.log(isPalindrome("something"));
 /* 
   Add your pseudocode here
+  take the word break each character of the word down 
+  put it in an array
+  then reverse the array 
+  join the array
+
 */
 
 /*
